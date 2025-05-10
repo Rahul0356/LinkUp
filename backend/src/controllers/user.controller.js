7e4a5329e63f321f0560c4be80598e98f5559c51
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-05-10 20:43:21 +0530

    implement the user controllers

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,25 @@ import httpStatus from "http-status";
 import {User} from "../models/User.model.js"
 import bcrypt, {hash} from "bcrypt";
 
+const login = async(req,res)=>{
+    const {username,password}= req.body;
+
+    if(!username  || !password){
+        return res.status(400).json({message:"Please Provide" })
+    }
+    try{
+
+    } catch(e){
+        
+    }
+}
+
+
+
+
+
+
+
 
 
 const register = async(req,res)=>{
@@ -14,7 +33,15 @@ if(existingUser){
     return res.status(httpStatus.FOUND).json({message:"User already exists"})
 }
 const hashedPassword = await bcrypt.hash(password, 10);
-}
-catch {
+const newUser = new User({
+    name: name,
+    username:username,
+    password:hashedPassword
+});
+await newUser.save();
+res.status(httpStatus.CREATED).json({message:"User Registered"})
 
+}catch (e){
+
+res.json({message:`Something went wrong${e}s`})
 }
