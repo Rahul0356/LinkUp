19a0508a6002fc48e4574f771d7af9e34ca890c6
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-05-10 15:19:12 +0530

    LinkUp project

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import {createServer} from 'http';
+
+
+import {Server} from 'socket.io';
+
+import mongoose from "mongoose";
+import {connectToServer}from "./controllers/socketManger.js";
+import cors from 'cors';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server)
+
+app.set("port",(process.env.PORT || 8000));
+
+app.get('/home', (req, res) => {
+    return res.json('Hello World!');
+}
+);
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://rahul90602092:GV9piVRIeVTDzeYV@cluster0.4qtzmit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    console.log(`Mongo Connected DB Host:${connectionDb.connection.host}`)
+    server.listen(app.get("port"),()=>{
+            console.log("Listening on PORT 8000")
+        });
+    };
+    
+    start();
